916bbd94eee27460add6ca4af7d5d5d9d8826a4b
Jeffribouchon <40675533+Jeffribouchon@users.noreply.github.com>
2025-09-02 14:24:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def vehicles():
     return render_template('vehicles.html', vehicle_data=vehicle_data)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
