996210bec9e6b56eb5839dbede7640f016deabbc
Jeffribouchon <40675533+Jeffribouchon@users.noreply.github.com>
2025-09-08 18:12:22 +0200

    Update matching_agent.py

--- modules/agents/matching/matching_agent.py
+++ modules/agents/matching/matching_agent.py
@@ -1,8 +1,8 @@
 # modules/agents/matching/matching_agent.py
 
 from modules.agents.base_agent import BaseAgent
-from modules.agents.contact_agent import ContactAgent
-from modules.agents.vehicle_agent import VehicleAgent
+from modules.agents.contacts.contact_agent import ContactAgent
+from modules.agents.vehicles.vehicle_agent import VehicleAgent
 
 
 class MatchingAgent(BaseAgent):
