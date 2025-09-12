c82ac5828cc801eb5ad9281f635cae122e64d7fc
Jeffribouchon <40675533+Jeffribouchon@users.noreply.github.com>
2025-09-12 14:09:00 +0200

    Update generate_url_agent.py

--- modules/agents/vehicles/generate_url_agent.py
+++ modules/agents/vehicles/generate_url_agent.py
@@ -166,22 +166,22 @@ def search(self, criteria: dict):
     urls = criteria
 
     # Scraping Le Bon Coin, La Centrale, Autoscout24 avec l'URL
-    if "Le Bon Coin" in criteria:
-        leboncoin_ads = scrape_leboncoin(criteria["Le Bon Coin"]["url"], limit=10)
-        ads.extend(leboncoin_ads)
+    # if "Le Bon Coin" in criteria:
+    #     leboncoin_ads = scrape_leboncoin(criteria["Le Bon Coin"]["url"], limit=10)
+    #     ads.extend(leboncoin_ads)
 
-    if "La Centrale" in criteria:
-        lacentrale_ads = scrape_lacentrale(criteria["La Centrale"]["url"], limit=10)
-        ads.extend(lacentrale_ads)
+    # if "La Centrale" in criteria:
+    #     lacentrale_ads = scrape_lacentrale(criteria["La Centrale"]["url"], limit=10)
+    #     ads.extend(lacentrale_ads)
 
-    if "Autoscout24" in criteria:
-        autoscout_ads = scrape_autoscout24(criteria["Autoscout24"]["url"], limit=10)
-        ads.extend(autoscout_ads)
+    # if "Autoscout24" in criteria:
+    #     autoscout_ads = scrape_autoscout24(criteria["Autoscout24"]["url"], limit=10)
+    #     ads.extend(autoscout_ads)
 
     # Scraping PlatformCars B2B avec les critères
-    if "PlatformCars B2B" in urls_json:
-        platformcars_ads = search_platformcars_b2b(criteria["PlatformCars B2B"]["criteria"], limit=10)
-        ads.extend(platformcars_ads)
+    # if "PlatformCars B2B" in urls_json:
+    #     platformcars_ads = search_platformcars_b2b(criteria["PlatformCars B2B"]["criteria"], limit=10)
+    #     ads.extend(platformcars_ads)
 
     # return criteria, ads
     return urls, ads
